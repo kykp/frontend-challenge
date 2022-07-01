f7e511c43c17c9d590fe91792890e5d34cad9555
kykp <kykp@yandex.ru>
2022-07-01 20:46:47 +0400

    changed preloading image

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,10 +1,95 @@
 import "./preloader.css"
 
+
 export const Preloader = () => {
   return (
-<div className="preloader">
-  <span className="preloader-span">..Loading...</span>
- 
-</div>
+    <section className='gallery'>
+      <div className="card skeleton">
+        <div className="card-image">
+        </div>
+      </div>
+
+      <div className="card skeleton">
+        <div className="card-image">
+        </div>
+      </div>
+
+      <div className="card skeleton">
+        <div className="card-image">
+        </div>
+      </div>
+
+      <div className="card skeleton">
+        <div className="card-image">
+        </div>
+      </div>
+
+      <div className="card skeleton">
+        <div className="card-image">
+        </div>
+      </div>
+
+      <div className="card skeleton">
+        <div className="card-image">
+        </div>
+      </div>
+
+      <div className="card skeleton">
+        <div className="card-image">
+        </div>
+      </div>
+
+      <div className="card skeleton">
+        <div className="card-image">
+        </div>
+      </div>
+
+      <div className="card skeleton">
+        <div className="card-image">
+        </div>
+      </div>
+
+      <div className="card skeleton">
+        <div className="card-image">
+        </div>
+      </div>
+
+      <div className="card skeleton">
+        <div className="card-image">
+        </div>
+      </div>
+
+      <div className="card skeleton">
+        <div className="card-image">
+        </div>
+      </div>
+
+      <div className="card skeleton">
+        <div className="card-image">
+        </div>
+      </div>
+
+      <div className="card skeleton">
+        <div className="card-image">
+        </div>
+      </div>
+
+      <div className="card skeleton">
+        <div className="card-image">
+        </div>
+      </div>
+      
+    </section>
   )
+
 }
+
+
+// export const Preloader = () => {
+//   return (
+// <div className="preloader">
+//   <span className="preloader-span">..Loading...</span>
+ 
+// </div>
+//   )
+// }
